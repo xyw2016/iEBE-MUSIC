95f050d3d97d9525778a2041cf5adcdd69b39f00
Xiang-Yu Wu <xiangyu.wu2@mail.mcgill.ca>
2025-06-25 16:46:17 -0400

    fix the bug in get_code_packages.sh

--- codes/get_code_packages.sh
+++ codes/get_code_packages.sh
@@ -9,27 +9,27 @@
 
 # # download IPGlasma
 #rm -fr ipglasma_code
-git clone --depth=1  https://github.com/xyw2016/ipglasma.git -b iebe_xyw_dev ipglasma_code
-commitHash="8e071daafbe45975ca7330bbc3d2f73b2124fdac"
-cd ipglasma_code 
-git checkout $commitHash
-rm -fr ipglasma_code/.git
-cd ..
-
-
- # download KoMPoST
-rm -fr kompost_code
-git clone --depth=1 https://github.com/xyw2016/KoMPoST.git -b xyw_iebe kompost_code
-commitHash="bc0355e5bbff7761054e8f8f3711d3e8ec2c321a"
-cd  kompost_code 
-git checkout $commitHash
-rm -fr kompost_code/.git
-cd ..
-
-# download part2s
-rm -fr part2s_code
-git clone --depth=1 https://github.com/xyw2016/pre_equlibrium_smash.git part2s_code
-rm -fr part2s_code/.git
+#git clone --depth=1  https://github.com/xyw2016/ipglasma.git -b iebe_xyw_dev ipglasma_code
+#commitHash="8e071daafbe45975ca7330bbc3d2f73b2124fdac"
+#cd ipglasma_code 
+#git checkout $commitHash
+#cd ..
+#rm -fr ipglasma_code/.git
+#
+#
+# # download KoMPoST
+#rm -fr kompost_code
+#git clone --depth=1 https://github.com/xyw2016/KoMPoST.git -b xyw_iebe kompost_code
+#commitHash="bc0355e5bbff7761054e8f8f3711d3e8ec2c321a"
+#cd  kompost_code 
+#git checkout $commitHash
+#cd ..
+#rm -fr kompost_code/.git
+#
+## download part2s
+#rm -fr part2s_code
+#git clone --depth=1 https://github.com/xyw2016/pre_equlibrium_smash.git part2s_code
+#rm -fr part2s_code/.git
 
 ## download MUSIC
 rm -fr MUSIC_code
@@ -37,15 +37,16 @@ git clone --depth=1  https://github.com/xyw2016/MUSIC.git -b polx MUSIC_code
 commitHash="af68a5d3f50beff78e117e87afdaaafc5922415a"
 cd  MUSIC_code 
 git checkout $commitHash
-rm -fr MUSIC_code/.git
 cd ..
-
-## download iSS particle sampler
+rm -fr MUSIC_code/.git
+#
+### download iSS particle sampler
 rm -fr iSS_code
 git clone --depth=1 https://github.com/xyw2016/iSS.git -b xyw_dev iSS_code
 commitHash="dd02b971f68514e83c7266191c81212df2c627f9"
 cd iSS_code 
 git checkout $commitHash
+cd ..
 rm -fr iSS_code/.git
 
 #
@@ -67,5 +68,5 @@ git clone --depth=1 https://github.com/xyw2016/hadronic_afterburner_toolkit.git 
 commitHash="bffe82fd9cea473e3a8e34c260ab4eeb0097c2d3"
 cd  hadronic_afterburner_toolkit_code
 git checkout $commitHash
-rm -fr hadronic_afterburner_toolkit_code2/.git
 cd ..
+rm -fr hadronic_afterburner_toolkit_code/.git
